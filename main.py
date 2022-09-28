b2867a0e940697896f9d0390f946111f0ce6fbeb
noy-sternlicht <noy.sternlicht@mail.huji.ac.il>
2022-09-28 13:06:10 +0300

    Sync with wandb, enable running without HPO
    This commit syncs the project with wandb, however,
    the syncing created two minor issues to be fixed later -
    
    1) There's no interface for naming hpo runs, solving this requires
    changing Pykeen.
    2) Wandb doesn't work with multiple hpo jobs. Hpo is reconfigured to run
    sequentially, wich will be fixed later on, if there's need.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import argparse
 import logging
 import os
+from datetime import datetime
 
 from pykeen.hpo import hpo_pipeline
-from pykeen.pipeline.api import pipeline_from_path, PipelineResult
+from pykeen.pipeline.api import pipeline
 
 ARTIFACTS_PATH = "./artifacts"
 
@@ -17,13 +18,42 @@ def init_logging() -> None:
     logging.getLogger().addHandler(logging.StreamHandler())
 
 
-def get_relevant_metrics(pipe_result: PipelineResult) -> str:
-    hits1 = pipe_result.get_metric("hits_at_1")
-    hits3 = pipe_result.get_metric("hits_at_3")
-    hits10 = pipe_result.get_metric("hits_at_10")
-    mrr = pipe_result.get_metric("inverse_harmonic_mean_rank")
+def train_with_hpo(kgc_model_name: str, dataset_name: str, artifacts_path: str) -> None:
+    logging.debug(f'Optimize hyper parameters for {kgc_model}')
+    hpo_result = hpo_pipeline(
+        n_trials=5,
+        model=kgc_model_name,
+        dataset=dataset_name,
+        stopper='early',  # Terminate unpromising trials
+        training_kwargs=dict(num_epochs=10),
+        result_tracker='wandb',
+        result_tracker_kwargs=dict(
+            project='primKgEval-testWandb',
+            tags=[f'{kgc_model}_{args.dataset}_hpo_' + datetime.now().strftime("%d/%m/%Y %H:%M:%S")],
+            reinit=True
+        ),
+    )
 
-    return f'H@1: {hits1} H@3: {hits3}, H@10: {hits10}, MRR: {mrr}'
+    hpo_result_path = os.path.join(artifacts_path, "hpo_result")
+    hpo_result.save_to_directory(hpo_result_path)
+
+
+def train(kgc_model_name: str, dataset_name: str, artifacts_path: str) -> None:
+    logging.debug(f'Train {kgc_model} model (no HPO)')
+    pip_result = pipeline(
+        model=kgc_model_name,
+        dataset=dataset_name,
+        result_tracker='wandb',
+        result_tracker_kwargs=dict(
+            project='primKgEval-testWandb',
+        ),
+        metadata=dict(
+            title=f'{kgc_model}_{args.dataset}_' + datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        )
+    )
+
+    result_path = os.path.join(artifacts_path, "result")
+    pip_result.save_to_directory(result_path)
 
 
 if __name__ == "__main__":
@@ -34,28 +64,13 @@ if __name__ == "__main__":
 
     parser.add_argument("--dataset", type=str)
     parser.add_argument("--models", type=str, nargs='+')
+    parser.add_argument("--hpo", action='store_true', default=False)
 
     args = parser.parse_args()
 
     for kgc_model in args.models:
-        logging.debug(f'Optimize hyper parameters for {kgc_model}')
-        hpo_result = hpo_pipeline(
-            n_trials=5,
-            model=kgc_model,
-            dataset=args.dataset,
-            device="cpu",
-            n_jobs=-1,  # Use all available CPUs
-            stopper='early',  # Terminate unpromising trials
-            training_kwargs=dict(num_epochs=10)
-        )
-
         model_artifacts_path = os.path.join(ARTIFACTS_PATH, kgc_model)
-        hpo_result_path = os.path.join(model_artifacts_path, "hpo_result")
-        hpo_result.save_to_directory(hpo_result_path)
-
-        logging.debug(f'Train optimized {kgc_model} model')
-        pip_result = pipeline_from_path(os.path.join(hpo_result_path, "best_pipeline/pipeline_config.json"))
-        pip_result.save_to_directory(os.path.join(model_artifacts_path, "optimal_pipline_result"))
-
-        eval_metrics = get_relevant_metrics(pip_result)
-        logging.info(f'[Model={kgc_model}] {eval_metrics}')
+        if args.hpo:
+            train_with_hpo(kgc_model, args.dataset, model_artifacts_path)
+        else:
+            train(kgc_model, args.dataset, model_artifacts_path)
