6e31d685580aa1f3d9b95e8ee3443575275860a6
noy-sternlicht <noy.sternlicht@mail.huji.ac.il>
2022-11-17 19:35:25 +0200

    Add LibKGE eval script for hitter

---
+++ libkge_run_eval_hitter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+#SBATCH --time=144:00:00
+#SBATCH --ntasks=5
+#SBATCH --gres=gpu:2,vmem:10g
+
+
+PWD=`pwd`
+echo $PWD
+activate () {
+    . $PWD/myenv2/bin/activate
+}
+
+
+activate
+NOW=$(date +"%F_%H-%M-%S")
+LOGFILE="artifacts/log-libkge-hitter-$NOW.log"
+libKGE/kge/kge/cli.py start libkge_configs/libkge_train_config_hitter.yaml --search.device_pool cuda:0,cuda:1 --search.num_workers 10 | tee "$LOGFILE"
